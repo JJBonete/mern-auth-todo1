831ac886ed4f9caebccea81291c4d0f69e088ed4
Jobert Janne Bonete <0812Neutral@gmail.com>
2023-03-18 16:31:40 +0800

    adding authReducer for signUp

---
+++ frontend/src/store/reducers/authReducer.js
@@ -0,0 +1,30 @@
+import { toast } from "react-toastify";
+import jwtDecode from "jwt-decode";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  name: null,
+  email: null,
+  _id: null,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SIGN_UP":
+      toast("Welcome!", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+      const user = jwtDecode(action.token);
+      return {
+        ...initialState,
+        token: action.token,
+        name: user.name,
+        email: user.email,
+        _id: user._id,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
